58cc98a86f171893d166dc80ec478a573552a2a7
ssaachin <s5225374@bournemouth.ac.uk>
2022-12-10 06:39:41 +0000

    7th commit

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import logging 
 import pymongo
 import json
+import datetime
 
 
 from flask import Flask, render_template,request 
 from pymongo import MongoClient
-from bson.json_util import dumps 
+from bson.json_util import dumps
+from google.auth.transport import requests 
+from google.cloud import datastore 
+import google.oauth2.id_token  
+
+firebase_request_adapter = requests.Request() 
+
+
+
+datastore_client = datastore.Client() 
 
 app = Flask(__name__)
+
+
+
+
+def store_time(email, dt): 
+    entity = datastore.Entity(key=datastore_client.key('User', email, 'visit')) 
+    entity.update({ 
+        'timestamp': dt 
+    }) 
+
+    datastore_client.put(entity)
+
+def fetch_times(email, limit): 
+    ancestor = datastore_client.key('User', email) 
+    query = datastore_client.query(kind='visit', ancestor=ancestor) 
+    query.order = ['-timestamp'] 
+
+    times = query.fetch(limit=limit) 
+    return times 
+
 
 cluster=MongoClient( "mongodb+srv://sachin2517:2517.Ylo@cluster0.mvn0mxf.mongodb.net/?retryWrites=true&w=majority") 
 db=cluster["test"] 
@@ -37,20 +67,50 @@ def get_mongodb_items():
 @app.route('/') 
 @app.route('/home') 
 def home(): 
-    jResponse=get_mongodb_items() 
-
-    data=json.loads(jResponse) 
-    return render_template('home.html', data=data) 
+    return render_template('home.html') 
 
 
 
 @app.route('/games') 
 def about(): 
-    return render_template('about.html')
+    jResponse=get_mongodb_items() 
+
+    data=json.loads(jResponse)
+    return render_template('Games.html', data=data)
 
 @app.route('/register') 
-def form(): 
-    return render_template('register.html') 
+def form():
+    id_token = request.cookies.get("token") 
+    error_message = None
+    claims = None
+    times = None
+
+    if id_token: 
+        try: 
+            # Verify the token against the Firebase Auth API. This example
+            # verifies the token on each page load. For improved performance,
+            # some applications may wish to cache results in an encrypted
+            # session store (see for instance
+            # http://flask.pocoo.org/docs/1.0/quickstart/#sessions).
+            claims = google.oauth2.id_token.verify_firebase_token( 
+                id_token, firebase_request_adapter) 
+
+            store_time(claims['email'], datetime.datetime.now()) 
+            times = fetch_times(claims['email'], 3) 
+
+            print(times)
+
+        except ValueError as exc: 
+            # This will be raised if the token is expired or any other
+            # verification checks fail.
+            error_message = str(exc) 
+
+ # Record and fetch the recent times a logged-in user has accessed
+ # the site. This is currently shared amongst all users, but will be
+ # individualized in a following step. 
+    return render_template(
+        'register.html',
+        user_data=claims, error_message=error_message, times=times) 
 # [END form] 
 
 # [START submitted]
